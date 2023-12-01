6ccd1b26e3513d93356f07815fe371a8a73e21c2
Peter Svensson <peter@sparetimecoders.com>
2023-12-01 13:58:03 +0100

    Revert "chore: update gobrew to not touch global version"
    This reverts commit 307bfaf030cc0285ccff69f92bbf930308c99524.

--- dot_config/direnv/lib/gobrew.sh
+++ dot_config/direnv/lib/gobrew.sh
@@ -1,8 +1,9 @@
 use_go() {
   local go_version=$1
 
-  out=$(gobrew install "${go_version}")
-  version=$(echo "${out}" | grep "Downloading version:" | rev | cut -d\  -f 1 | rev)
-  export PATH=~/.gobrew/versions/${version}/go/bin:${PATH}
-  export GOROOT=~/.gobrew/versions/${version}/go
+  if [ "mod" == "${go_version}" ]; then
+    gobrew use mod
+  else
+    gobrew use "${go_version}"
+  fi
 }
