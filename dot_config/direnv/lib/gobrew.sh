307bfaf030cc0285ccff69f92bbf930308c99524
Peter Svensson <peter@sparetimecoders.com>
2023-12-01 11:52:55 +0100

    chore: update gobrew to not touch global version

--- dot_config/direnv/lib/gobrew.sh
+++ dot_config/direnv/lib/gobrew.sh
@@ -1,9 +1,8 @@
 use_go() {
   local go_version=$1
 
-  if [ "mod" == "${go_version}" ]; then
-    gobrew use mod
-  else
-    gobrew use "${go_version}"
-  fi
+  out=$(gobrew install "${go_version}")
+  version=$(echo "${out}" | grep "Downloading version:" | rev | cut -d\  -f 1 | rev)
+  export PATH=~/.gobrew/versions/${version}/go/bin:${PATH}
+  export GOROOT=~/.gobrew/versions/${version}/go
 }
