833352172ac23531a077bc67cab3cbf3e7b28da0
Peter Svensson <peter@sparetimecoders.com>
2024-01-09 11:45:25 +0100

    chore: gobrew

--- dot_config/direnv/lib/gobrew.sh
+++ dot_config/direnv/lib/gobrew.sh
@@ -2,6 +2,9 @@ use_go() {
   local go_version=$1
   out=$(gobrew install "${go_version}")
   version=$(echo "${out}" | grep "Downloading version:" | rev | cut -d\  -f 1 | rev)
+  if [[ -z $version ]]; then
+    version=$(echo "${out}" | grep "Version: " | cut -d\  -f 4)
+  fi
   export PATH=~/.gobrew/versions/${version}/go/bin:${PATH}
   export GOROOT=~/.gobrew/versions/${version}/go
 }
