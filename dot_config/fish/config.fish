09ab15e9463c940e4bf157a561e662a07338f76a
Peter Svensson <peter@sparetimecoders.com>
2025-11-13 09:37:52 +0100

    chore: fix tmux folder on restore

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,7 +6,10 @@
 # https://github.com/fish-shell/fish-shell/blob/da32b6c172dcfe54c9dc4f19e46f35680fc8a91a/share/config.fish#L257-L269
 
 if status is-interactive
-  cd $HOME
+  # Only cd to HOME if not in tmux (to preserve tmux-resurrect working directories)
+  if not set -q TMUX
+    cd $HOME
+  end
 end
 
 #
