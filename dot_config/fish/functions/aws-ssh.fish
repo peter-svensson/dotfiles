979406b3c4c9059b8dfe7fcf290fdeffdf931af9
Peter Svensson <peter@sparetimecoders.com>
2026-02-27 10:28:57 +0100

    feat: default sudo to root for aws-ssh

--- dot_config/fish/functions/aws-ssh.fish
+++ dot_config/fish/functions/aws-ssh.fish
@@ -2,9 +2,13 @@ function aws-ssh --description "Start an SSM session to an EC2 instance"
     set -l instance_id $argv[1]
 
     if test -z "$instance_id"
-        echo "Usage: aws-ssh <instance-id>"
+        echo "Usage: aws-ssh <instance-id> [--no-root]"
         return 1
     end
 
-    aws ssm start-session --target $instance_id
+    if contains -- --no-root $argv
+        aws ssm start-session --target $instance_id
+    else
+        aws ssm start-session --target $instance_id --document-name AWS-StartInteractiveCommand --parameters command="sudo su -"
+    end
 end
