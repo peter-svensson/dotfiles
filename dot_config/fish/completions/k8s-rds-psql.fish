ab6009fbc3c7141aaf95f277dd9d51e5f8d72fb6
Peter Svensson <peter@sparetimecoders.com>
2026-03-04 09:50:06 +0100

    chore: add psql tunnel connection function

---
+++ dot_config/fish/completions/k8s-rds-psql.fish
@@ -0,0 +1,20 @@
+# Completions for k8s-rds-psql
+
+function __k8s_rds_psql_contexts
+    kubectl config get-contexts -o name 2>/dev/null
+end
+
+function __k8s_rds_psql_namespaces
+    set -l ctx (commandline -opc | string match -r -- '--context\s+(\S+)' | tail -1)
+    if test -n "$ctx"
+        kubectl --context $ctx get namespaces -o jsonpath='{.items[*].metadata.name}' 2>/dev/null | string split ' '
+    end
+end
+
+# No file completion by default
+complete -c k8s-rds-psql -f
+
+# Flags
+complete -c k8s-rds-psql -s c -l context -d 'Kubernetes context (required)' -r -a '(__k8s_rds_psql_contexts)'
+complete -c k8s-rds-psql -s p -l port -d 'Local port (default: 35432)' -r
+complete -c k8s-rds-psql -s n -l namespace -d 'Kubernetes namespace (default: same as name)' -r -a '(__k8s_rds_psql_namespaces)'
---
+++ dot_config/fish/functions/k8s-rds-psql.fish
@@ -0,0 +1,40 @@
+function k8s-rds-psql --description "Connect to RDS PostgreSQL via tunnel using k8s secret credentials"
+    argparse 'p/port=!_validate_int --min 1 --max 65535' 'c/context=' 'n/namespace=' -- $argv
+    or return 1
+
+    set -l name $argv[1]
+
+    if test -z "$name"
+        echo "Usage: k8s-rds-psql <name> --context <cluster> [--port <local-port>] [--namespace <ns>]"
+        echo ""
+        echo "  <name>        Database name, username, and secret name"
+        echo "  --context     Kubernetes context (required)"
+        echo "  --port        Local port (default: 35432, must match k8s-rds-tunnel)"
+        echo "  --namespace   Kubernetes namespace (default: same as name)"
+        return 1
+    end
+
+    if not set -q _flag_context
+        echo (set_color red)"--context is required"(set_color normal)
+        echo "Usage: k8s-rds-psql <name> --context <cluster>"
+        return 1
+    end
+
+    set -l local_port (if set -q _flag_port; echo $_flag_port; else; echo 35432; end)
+    set -l namespace (if set -q _flag_namespace; echo $_flag_namespace; else; echo $name; end)
+    set -l db_name (string replace -a '-' '_' $name)
+
+    # Get password from k8s secret
+    set -l secret_name "$name-secrets"
+    echo "Looking up password from secret '$secret_name' in namespace '$namespace'..."
+    set -l password (kubectl --context $_flag_context -n $namespace get secret $secret_name -o jsonpath='{.data.DB_PASSWORD}' 2>&1)
+    if test $status -ne 0
+        echo (set_color red)"Failed to get secret: $password"(set_color normal)
+        return 1
+    end
+
+    set password (echo $password | base64 --decode)
+
+    echo "Connecting to database '$db_name' as user '$db_name' on localhost:$local_port..."
+    PGPASSWORD=$password psql -h localhost -p $local_port -U $db_name -d $db_name
+end
