be59a3436c89edccd5dafe7511297a7d81ab7842
Peter Svensson <peter@sparetimecoders.com>
2026-04-08 23:35:38 +0200

    feat: add pre-commit to prek abbreviation

--- dot_config/fish/conf.d/abbrs.fish
+++ dot_config/fish/conf.d/abbrs.fish
@@ -13,5 +13,7 @@ abbr -a -- k kubectl
 abbr -a -- kc kubectl --context
 abbr -a -- sc stern --context
 
+abbr -a -- pre-commit prek
+
 abbr -a -- gup git up
 abbr -a -- ggone "git branch -vv | grep ': gone]' | awk '{print \$1}' | xargs git branch -D"
